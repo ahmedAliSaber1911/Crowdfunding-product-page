e7043506be33cf0fc4cd97b460edc5b68f34641c
Ahmed Saber <ahmedalisaber965@gmail.com>
2022-11-01 01:44:57 +0200

    finish the section and the media

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let hamburger = document.querySelector('.hamburger');
 let closeMenu = document.querySelector('.close-menu');
 let navlist = document.querySelector('.list');
-
-
+let bookMark = document.querySelector('.bookmark')
+let iconBookmark = document.querySelector('.icon-bookmark')
 hamburger.addEventListener('click' , ()=>{  
 hamburger.style.display = 'none';
 closeMenu.style.display = 'block';
@@ -13,5 +13,9 @@ closeMenu.addEventListener('click' , ()=>{
     hamburger.style.display = 'block';
     closeMenu.style.display = 'none';
     navlist.style.display = 'none';
-
+})
+bookMark.addEventListener('click',()=>{
+    bookMark.classList.toggle('bookmarkActive');
+    
+    iconBookmark.classList.toggle('icon-active')
 })
